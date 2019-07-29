933d5997f5e9d38550335f032c14ee43754293dd
sky yu <13761645531@163.com>
2019-07-29 15:05:32 +0800

    yulu commit1.1

---
+++ pinyougou58/pinyougou-user-web/src/main/webapp/js/controller/homePersonCollect.js
@@ -0,0 +1,37 @@
+﻿var app = new Vue({
+    el: "#app",
+    data: {
+        pages:15,
+        pageNo:1,
+        list:[],
+        entity:{},
+        smsCode:'',
+        ids:[],
+        searchEntity:{},
+        cartList:[],
+        loginName:""
+    },
+    methods: {
+        /**
+         * 查看购物车
+         */
+        findCartList: function () {
+            axios.get('/user/findCartList.shtml').then(function(response){
+                app.cartList=response.data;
+            })
+        },
+
+        //获取用户名
+        getName:function(){
+          axios.get('/login/name.shtml').then(function(response){
+              app.loginName=response.data;
+          })
+        }
+    },
+    //钩子函数 初始化了事件和
+    created: function () {
+        this.getName();
+        this.findCartList();
+    }
+
+})
