0caa138ffe93cb5bc4d72c2e28341667b4203b1a
sky yu <13761645531@163.com>
2019-07-29 15:31:35 +0800

    yulu commit1.3

--- pinyougou58/pinyougou-seckill-service/src/main/java/com/pinyougou/seckill/listener/PageMessageListener.java
+++ pinyougou58/pinyougou-seckill-service/src/main/java/com/pinyougou/seckill/listener/PageMessageListener.java
@@ -36,16 +36,29 @@ public class PageMessageListener implements MessageListenerConcurrently {
     @Override
     public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
         try {
-            //取出消息
-            for (MessageExt messageExt : list) {
-                byte[] body = messageExt.getBody();
-                String s = new String(body);
-                MessageInfo messageInfo = JSON.parseObject(s, MessageInfo.class);
-                if (messageInfo!=null&&messageInfo.getMethod()==MessageInfo.METHOD_ADD) {
-                    Long[] longs = JSON.parseObject(messageInfo.getContext().toString(), Long[].class);
-                    for (Long aLong : longs) {
+            if (list != null && list.size() > 0) {
+                //取出消息
+                for (MessageExt messageExt : list) {
+                    byte[] body = messageExt.getBody();
+                    String s = new String(body);
+                    MessageInfo messageInfo = JSON.parseObject(s, MessageInfo.class);
+                    //获取id
+                    if (messageInfo != null) {
+                        Long[] longs = JSON.parseObject(messageInfo.getContext().toString(), Long[].class);
                         //生成静态页面
-                        genHTML("item.ftl",aLong);
+                        if (messageInfo.getMethod() == MessageInfo.METHOD_ADD) {
+                            for (Long aLong : longs) {
+                                genHTML("item.ftl", aLong);
+                            }
+                        }
+
+                        //删除静态页面
+                        if (messageInfo.getMethod() == MessageInfo.METHOD_DELETE) {
+                            for (Long aLong : longs) {
+                                File file = new File("E:\\ideaworkspace\\pinyougou58\\pinyougou-seckill-web\\src\\main\\webapp\\" + aLong + ".html");
+                                file.delete();
+                            }
+                        }
                     }
                 }
             }
@@ -70,11 +83,13 @@ public class PageMessageListener implements MessageListenerConcurrently {
             //数据源
             TbSeckillGoods seckillGoods = seckillGoodsMapper.selectByPrimaryKey(id);
             Map map = new HashMap<>();
-            map.put("seckillGoods",seckillGoods);
+            map.put("seckillGoods", seckillGoods);
             //创建静态页面的流
-            writer = new FileWriter(new File(PageDir+id+".html"));
+            writer = new FileWriter(new File(PageDir + id + ".html"));
             //生成静态页面
-            template.process(map,writer);
+            template.process(map, writer);
+            //template.dump();
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
